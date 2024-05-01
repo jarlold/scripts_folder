11ab9ad586851f92cc8e2ea5b1ba8d95232a06fc
jarlold <jarlold@protonmail.com>
2024-05-01 16:55:50 -0400

    Improved emoji script to include searching and help page.

--- hexchat_emojis.py
+++ hexchat_emojis.py
@@ -1,60 +1,101 @@
 __module_name__ = "Vomit"
-__module_version__ = "1.0"
+__module_version__ = "1.1"
 __module_description__ = "Add vomit emoji command and other emojis too"
 
 import hexchat
 import emoji
 
+wrong = lambda : print("You used my command wrong!")
+
+def search_emoji(word, word_eol, userdata):
+    if len(word) <= 1:
+        wrong()
+        return hexchat.EAT_ALL
+
+    found_one = False
+    search_term = word[1]
+    for e in emoji.EMOJI_DATA:
+        if search_term in emoji.EMOJI_DATA[e]["en"]:
+            print(e, "is called", emoji.EMOJI_DATA[e]["en"])
+            found_one = True
+
+    if not found_one:
+        print("Couldn't find any emoji called that")
+
+    return hexchat.EAT_ALL
+
+
 def vomit(word, word_eol, userdata):
-    if word[0] == "vomit":
-        # Get the amount of vomits to vomit
-        if len(word) > 1:
-            try:
-                amount = int(word[1])
-            except ValueError:
-                print("You used my command wrong")
-        else:
-            amount = 1
+    # Get the amount of vomits to vomit
+    if len(word) > 1:
+        try:
+            amount = int(word[1])
+        except ValueError:
+            wrong()
+            return hexchat.EAT_ALL
+    else:
+        amount = 1
 
-        # Make sure its somewhat reasonable
-        if amount < 1 or amount > 256:
-            print("You used my command wrong!")
-            return
+    # Make sure its somewhat reasonable
+    if amount < 1 or amount > 256:
+        wrong()
+        return hexchat.EAT_ALL
 
-        # Send them in chat
-        hexchat.command("MSG {} {}".format(
-            hexchat.get_info("channel"), amount*"🤮"))
+    # Send them in chat
+    hexchat.command("MSG {} {}".format(
+        hexchat.get_info("channel"), amount*"🤮"))
+
+    return hexchat.EAT_ALL
+
 
 def any_emoji(word, word_eol, userdata):
     if len(word) < 2:
-        print("You used my command wrong")
+        wrong()
+        return hexchat.EAT_ALL
 
-    if word[0] == "emoji":
-        # Get the amount of vomits to vomit
-        if len(word) > 2:
-            try:
-                amount = int(word[2])
-            except ValueError:
-                print("You used my command wrong")
-        else:
-            amount = 1
+    # Get the amount of emojis to vomit
+    if len(word) > 2:
+        try:
+            amount = int(word[2])
+        except ValueError:
+            wrong()
+            return hexchat.EAT_ALL
+    else:
+        amount = 1
 
-        # Make sure its somewhat reasonable
-        if amount < 1 or amount > 256:
-            print("You used my command wrong!")
-            return
+    # Make sure its somewhat reasonable
+    if amount < 1 or amount > 256:
+        wrong()
+        return hexchat.EAT_ALL
 
-        # Send them in chat
-        selected_emoji = emoji.emojize(word[1])
-        if selected_emoji == word[1]:
-            print("No such emoji")
-        else:
-            hexchat.command("MSG {} {}".format(
-                hexchat.get_info("channel"),
-                amount*selected_emoji)
-            )
+    # If the user forgot the colons (:) on either side
+    # of the emoji name, we'll forgive them.
+    if word[1].count(":") != 2:
+        word[1] = ":" + word[1].strip(":")+ ":"
+
+    # Send them in chat
+    selected_emoji = emoji.emojize(word[1])
+    if selected_emoji == word[1]:
+        print("No such emoji")
+    else:
+        hexchat.command("MSG {} {}".format(
+            hexchat.get_info("channel"),
+            amount*selected_emoji)
+        )
+    return hexchat.EAT_ALL
+
+
+def emoji_help(word, word_eol, userdata):
+    print("Vomit.py Emoji Script:")
+    print("/vomit [amount] \t use my favorite emoji [amount] times")
+    print("/emoji [emoji_name] [amount] \t use [emoji_name] [amount] times.")
+    print("/search_emoji [search_term] \t search an emoji name")
+    print("Made with ❤️ by Jarlold")
+    return hexchat.EAT_ALL
 
 
 hexchat.hook_command("vomit", vomit)
 hexchat.hook_command("emoji", any_emoji)
+hexchat.hook_command("search_emoji", search_emoji)
+hexchat.hook_command("emoji_help", emoji_help)
 
